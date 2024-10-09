1b713764082a3e55f762450ac7cd65a00c328bc8
preykriti <prakriteeshrestha2@gmail.com>
2024-10-09 21:40:11 +0545

    logic to validate user

--- inotebook/backend/models/User.js
+++ inotebook/backend/models/User.js
@@ -1,25 +1,25 @@
 import mongoose from "mongoose";
 
 const UserSchema = mongoose.Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    date:{
-        type: Date,
-        default: Date.now
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const User = mongoose.model("user",UserSchema);
-User.createIndexes();
-export default User
+const User = mongoose.model("user", UserSchema);
+// User.createIndexes();
+export default User;
--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -5,7 +5,7 @@ import { body, validationResult } from "express-validator";
 //import User from "../models/User";
 
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "enter a valid name").isLength({ min: 3 }),
     body("email", "enter a valid email").isEmail(),
@@ -13,20 +13,37 @@ router.post(
       min: 8,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    //If there are errors, return bad request and errors
     console.log("Route hit");
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    }).then((user) => res.json(user)).catch(err=>{console.log(err)
-        res.json({error: "please enter a unique value for email"})
-    });
-    
+
+    //check if the email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "This email already existss" });
+      }
+      //create a new user
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      });
+      res.json({ successful: "yes" });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("some error occured");
+    }
+
+    //.then((user) => res.json(user))
+    // .catch((err) => {
+    // console.log(err);
+    // res.json({ error: "please enter a unique value for email" });
+    // });
   }
 );
 
